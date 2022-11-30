1b9cbbf720c1e2da53dc7b0cd32501aabe2f3efd
Raman Singh <mrramansingh30@gmail.com>
2022-11-29 20:45:09 -0500

    First commit

---
+++ Project 2/AddLocationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddLocationViewController.swift
+//  Project 2
+//
+//  Created by Raman Singh on 2022-11-29.
+//
+
+import UIKit
+
+class AddLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
